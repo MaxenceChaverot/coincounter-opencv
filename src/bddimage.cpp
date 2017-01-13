869269cddf53b62500b3d08b3e219238d924032a
Maxence CHAVEROT <chaverot.maxence@gmail.com>
2017-01-13 11:25:05 +0100

    Update Regex for glob function

--- src/bddimage.cpp
+++ src/bddimage.cpp
@@ -12,11 +12,11 @@ std::string BddImage::cleanPathString(std::string path){
 	std::ostringstream s;
 	s << path;
 
-	//If the path string doesn't end by '/' 
+	//If the path string doesn't end by '/' -> add '/' to the string 
 	if(path[path.size()-1] != '/') s <<"/";
 	
 	//Add regex for glob function
-	s <<"*_cent.png";
+	s <<"*_cent*.png";
 	std::string str (s.str());
 	return str;
 }
@@ -29,6 +29,8 @@ void BddImage::loadImg(){
 	//cv::glob method to find all the image
 	glob(pathToBddFolder, fn, true);
 	
+	std::cout<<"Bdd will be filed with "<<fn.size()<<" pictures"<<std::endl;
+
 	for(int i = 0; i < fn.size(); ++i){
 		ConteneurImage contImg(fn[i]);
 		int label = retrieveLabelInFileName(fn[i]);
@@ -56,7 +58,7 @@ void BddImage::fillKpAndDescriptor(Comparator comparator){
 int BddImage::retrieveLabelInFileName(std::string pathToImg){
 
 	int pos = 0;
-	int pos_end = pathToImg.find("_cent.png");
+	int pos_end = pathToImg.find("_cent");
 
 	std::ostringstream s;
 
